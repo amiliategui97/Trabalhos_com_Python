0aceb8042dcd2fbec5d67c952259ecfa346ef337
Rafaela Mariano Sanchez Amiliátegui <amiliateguisanchez@gmail.com>
2024-08-22 21:35:19 -0300

    Add files via upload

---
+++ Aulas-de-Python/Strings-P1.py
@@ -0,0 +1,16 @@
+#Strings P1
+curso = "Curso de Python"
+
+print(curso[9:15])
+
+#Lenuma função embutida que retorna o número de elementos em um objeto
+print("tamanho: " + str(len(curso)))
+
+#Função strip remove todos os espaços em branco no início e no final de uma string
+print(curso.strip())
+
+# retorna a string com todos os caracteres convertidos para minúsculas.
+print(curso.lower().strip())
+
+#Funçao upper converte todas as letras minúsculas numa string para maiúsculas
+print(curso.upper())
